c44921859cacab281d4a90339e30ab47f7f85a3b
Mário Queiroz <mario.al.queiroz@gmail.com>
2020-12-19 17:34:42 -0300

    Add helper functions, docs, and tests

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,156 @@
 //! # FR Data Downloader
 //!
-//! Utility to download FR Data in batch.
+//! Utility to download FR Data from public API.
+//!
+//! The motivation to crate this tool was the need to conduct routine audits
+//! on customer databases to ensure that all government requirements were met.
+//!
+//! Among the things that still need to be done are:
+//!
+//! - Correctly handle the requests with the API key;
+//! - Generate the CSV summary from the downloaded data;
+//! - Validate the FR.
 
 #[macro_use]
 extern crate lazy_static;
 
-use std::fs::File;
+use std::fs::{metadata, File};
 use std::io::{BufRead, BufReader, Lines};
 
-use dotenv;
+use filetime::FileTime;
 use regex::Regex;
+use walkdir::WalkDir;
 
 // Added this macro to be able to have `static`s with data loaded from `dotenv` at runtime.
 // Trying to use `const` in this case produces the following error:
 // `calls in constants are limited to constant functions, tuple structs and tuple variants`
 lazy_static! {
+    /// URL to get data from.
     static ref API_URL: String = dotenv::var("API_URL").expect("Unable to get API URL.");
-    static ref MARGIN_OF_ERROR: String = dotenv::var("MARGIN_OF_ERROR").unwrap_or("0".to_string());
+
+    /// Margin of error in seconds to get the data, respecting the limits of the API.
+    static ref MARGIN_OF_ERROR: String =
+        dotenv::var("MARGIN_OF_ERROR").unwrap_or_else(|_| "0".to_string());
+
+    /// Limit of HTTP requests per minute according to the contracted plan.
     static ref LIMIT_PER_MINUTE: String =
-        dotenv::var("LIMIT_PER_MINUTE").unwrap_or("3".to_string());
+        dotenv::var("LIMIT_PER_MINUTE").unwrap_or_else(|_| "3".to_string());
+
+    /// Interval, in seconds, between each HTTP request according to the values specified
+    /// in `LIMIT_PER_MINUTE` and `MARGIN_OF_ERROR`.
     static ref INTERVAL: f32 =
         60.0 / LIMIT_PER_MINUTE.parse::<f32>().unwrap() + MARGIN_OF_ERROR.parse::<f32>().unwrap();
-    static ref INPUT_FILE: String = dotenv::var("INPUT_FILE").unwrap_or("./input.txt".to_string());
+
+    /// File containing the FR, separated by new line.
+    static ref INPUT_FILE: String =
+        dotenv::var("INPUT_FILE").unwrap_or_else(|_| "./input.txt".to_string());
+
+    /// Path of the folder to save the data obtained from the API.
+    static ref OUTPUT_FOLDER: String =
+        dotenv::var("OUTPUT_FOLDER").unwrap_or_else(|_| "./downloads/".to_string());
+
+    /// Maximum age of file to determine if it needs to be downloaded again.
+    ///
+    /// 30 days seems to be a good interval, since the FR data doesn't change
+    /// so frequently, and this way we do not need to make so many requests to
+    /// the server, since different customers may have associations with FRs
+    /// from others.
+    static ref MAXIMUM_AGE: i64 =
+        dotenv::var("MAXIMUM_AGE").unwrap_or_else(|_| "30".to_string()).parse::<i64>().unwrap();
 }
 
-fn save(name: &str, data: &str) {
-    match std::fs::create_dir(".downloads/") {
-        Ok(_) => println!("Creating directory."),
-        Err(_) => println!("Directory already exists."),
+/// Create output folder in the current directory if not exists.
+/// Do nothing otherwise.
+fn create_output_folder() {
+    if std::fs::create_dir(OUTPUT_FOLDER.as_str()).is_ok() {}
+}
+
+/// Read RFs from file line by line.
+fn get_frs_from_file() -> Lines<BufReader<File>> {
+    BufReader::new(File::open(INPUT_FILE.as_str()).unwrap()).lines()
+}
+
+/// Remove all non-numeric characters from the FR so it can be used to make the
+/// HTTP request to the API.
+fn normalize_fr(fr: &str) -> String {
+    Regex::new(r"[^0-9]")
+        .unwrap()
+        .replace_all(&fr, "")
+        .to_string()
+}
+
+#[test]
+fn normalized() {
+    assert_eq!(normalize_fr(""), "");
+    assert_eq!(normalize_fr("12"), "12");
+    assert_eq!(normalize_fr("no numbers"), "");
+    assert_eq!(normalize_fr(" as-12.df "), "12");
+}
+
+/// Check if the specified FR already has the respective file in the `OUTPUT_FOLDER`.
+fn is_downloaded(fr: &str) -> bool {
+    for entry in WalkDir::new(OUTPUT_FOLDER.to_string()) {
+        let path = entry.unwrap().path().to_str().unwrap().to_owned();
+        if path.contains(fr) {
+            return true;
+        }
+    }
+    false
+}
+
+/// Check if the downloaded file is older than the specified `MAXIMUM_AGE`.
+/// If so, it needs to be downloaded again.
+fn is_old(age_of_file: i64) -> bool {
+    age_of_file > *MAXIMUM_AGE
+}
+
+#[test]
+fn test_age() {
+    if *MAXIMUM_AGE == 30 {
+        assert_eq!(is_old(1), false);
+        assert_eq!(is_old(30), false);
+        assert_eq!(is_old(31), true);
     }
 }
 
-fn get_lines_from_file() -> Lines<BufReader<File>> {
-    match file = File::open(INPUT_FILE.as_str()) {
-        Some(file) => file,
-        None => eprintln!("Unable to open the file."),
-    };
-    let reader = BufReader::new(file);
-    reader.lines()
+/// Get the age of the file as day.
+fn get_age_of_file(file_name: &str) -> i64 {
+    let metadata = metadata(file_name).unwrap();
+
+    // Here we are getting the modification date because, as the `filetime`
+    // documentation, _not all Unix platforms have this field available and
+    // may return None in some circumstances_.
+    age_in_days(
+        FileTime::now().seconds() - FileTime::from_last_modification_time(&metadata).seconds(),
+    )
 }
 
-fn normalize_fr(fr: &str) -> String {
-    //! Remove all non-numeric characters from the FR.
-    let regex = Regex::new(r"[^0-9]").unwrap();
-    regex.replace_all(&fr, "").to_string()
+/// Helper function to convert the timestamp as day.
+fn age_in_days(seconds: i64) -> i64 {
+    let age_in_minutes = seconds / 60;
+    let age_in_hours = age_in_minutes / 60;
+    age_in_hours / 24
 }
 
+#[test]
+fn day() {
+    let sec_day = 86400;
+    assert_eq!(age_in_days(sec_day - 100), 0);
+    assert_eq!(age_in_days(sec_day), 1);
+    assert_eq!(age_in_days(sec_day + 100), 1);
+    assert_eq!(age_in_days(sec_day * 2), 2);
+    assert_eq!(age_in_days(sec_day * 2 + 100), 2);
+}
+
+#[doc(hidden)]
 fn main() {
-    for line in get_lines_from_file() {
-        println!("{:?}", normalize_fr(&line.unwrap()));
+    create_output_folder();
+    for fr in get_frs_from_file() {
+        let normalized_fr = normalize_fr(&fr.unwrap());
+        let api_call = format!("{}{}", API_URL.to_string(), normalized_fr);
+        let file_path = format!("{}{}.json", OUTPUT_FOLDER.to_string(), normalized_fr);
+
+        println!("{:?}", is_downloaded(&normalized_fr));
+        println!("{:?}", is_old(get_age_of_file(&file_path)));
     }
 }
