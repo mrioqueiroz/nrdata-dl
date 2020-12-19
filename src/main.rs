4be47cd2450afec07f7eb805a0f87a49d7c26dd8
Mário Queiroz <mario.al.queiroz@gmail.com>
2020-12-19 03:07:20 -0300

    Add .env file and ability to load data to statics
    Also add .env to .gitignore and add new dependencies to the project.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+//! # FR Data Downloader
+//!
+//! Utility to download FR Data in batch.
+
+#[macro_use]
+extern crate lazy_static;
+
+use dotenv;
+
+// Added this macro to be able to have `static`s with data loaded from `dotenv` at runtime.
+// Trying to use `const` in this case produces the following error:
+// `calls in constants are limited to constant functions, tuple structs and tuple variants`
+lazy_static! {
+    static ref API_URL: String = dotenv::var("API_URL").expect("Unable to get API URL.");
+    static ref MARGIN_OF_ERROR: String = dotenv::var("MARGIN_OF_ERROR").unwrap_or("0".to_string());
+    static ref LIMIT_PER_MINUTE: String =
+        dotenv::var("LIMIT_PER_MINUTE").unwrap_or("3".to_string());
+    static ref INTERVAL: f32 =
+        60.0 / LIMIT_PER_MINUTE.parse::<f32>().unwrap() + MARGIN_OF_ERROR.parse::<f32>().unwrap();
+}
+
 fn main() {
 
 }
